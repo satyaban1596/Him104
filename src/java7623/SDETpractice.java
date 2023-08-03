6a64faf206cf2b60dd7d675221fe3d6982c3bb68
satyaban1596 <satyabanmahunta123@gmail.com>
2023-08-03 18:30:29 +0530

    add to repo

---
+++ src/java7623/SDETpractice.java
@@ -0,0 +1,13 @@
+package java7623;
+
+public class SDETpractice {
+
+	public static void main(String[] args) {
+		int a[] = { 100, 200, 300, 400, 500 };
+		for (int i : a) {
+			System.out.println(i);
+
+		}
+
+	}
+}
